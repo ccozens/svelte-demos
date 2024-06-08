1a906c75cac7f65fc50517a21e892d61f48b8bdd
ccozens <ccozens@gmail.com>
2024-06-08 14:47:34 +0100

    added nav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let pages: string[] = ["Home", "About", "Contact"];
+</script>
+
+<nav class="flex gap-4 bg-blue-900">
+    {#each pages as page}
+        <a class="rounded-lg p-3 hover:bg-indigo-200 hover:text-yellow-800 focus:bg-yellow-50" href="/{page.toLowerCase()}">{page}</a>
+    {/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import type { Snippet } from 'svelte';
 	let { children } = $props<{ children: Snippet }>();
+
+	import Nav from '$lib/components/Nav.svelte';
 </script>
 
+<Nav />
 {@render children()}
