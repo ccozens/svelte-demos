bc04a584461725d86c11b83b09adc29646f46e42
ccozens <ccozens@gmail.com>
2024-06-08 14:40:33 +0100

    svelte 5 with tailwind and demo component initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+
+    function decrement() {
+        count -= 1;
+    }
+
+    let double = $derived(count *2);
+</script>
+
+
+<p>count: {count}</p>
+<p>double: {double}</p>
+<button onclick={increment}>+</button>
+<button onclick={decrement}>-</button>
